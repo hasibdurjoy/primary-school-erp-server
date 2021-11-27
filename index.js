2ade37086fb3d06df410b8fffc1d1ab49a1cdfa8
hasibdurjoy <hrd2571@gmail.com>
2021-11-27 19:55:53 +0600

    update

--- index.js
+++ index.js
@@ -49,40 +49,7 @@ async function run() {
             res.json(product);
         });
 
-        /* //GET  PRODUCTS BY ID
-        app.get('/manageProducts/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) }
-            const product = await productCollection.findOne(query);
-            res.json(product);
-        });
 
-
-        //GET  USERS WHO ARE ADMIN
-        app.get('/users', async (req, res) => {
-            const role = req.query.role;
-
-            const query = { role: role }
-
-            const cursor = usersCollection.find(query);
-            const users = await cursor.toArray();
-            res.json(users);
-        });
-
-        //GET  USERS BY IS
-        app.get('/users/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            const user = await usersCollection.findOne(query);
-            let isAdmin = false;
-            console.log(user?.role);
-            if (user?.role === 'admin') {
-                isAdmin = true;
-            }
-            res.json({ admin: isAdmin });
-        }) */
-
-        //POST
 
         //ADD NEW STUDENT
         app.post('/allStudents', async (req, res) => {
@@ -90,6 +57,23 @@ async function run() {
             const result = await studentCollection.insertOne(student);
             res.json(result)
         });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result)
+        });
+
+        //ADD NEW USER IF NOT HAVE
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
 
         app.put('/allStudents/:id', async (req, res) => {
             const id = req.params.id;
@@ -109,25 +93,12 @@ async function run() {
             res.json(result);
         });
 
-        /* //ADD NEW USER
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            res.json(result)
-        });
+        /* 
 
 
         //PUT
 
-        //ADD NEW USER IF NOT HAVE
-        app.put('/users', async (req, res) => {
-            const user = req.body;
-            const filter = { email: user.email };
-            const options = { upsert: true };
-            const updateDoc = { $set: user };
-            const result = await usersCollection.updateOne(filter, updateDoc, options);
-            res.json(result);
-        });
+        
 
         //MAKE ADMIN
         app.put('/users/admin', async (req, res) => {
@@ -137,30 +108,7 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
-
-
-
-        //UPDATE PRODUCT
-        app.put('/products/:id', async (req, res) => {
-            const id = req.params.id;
-            const product = req.body;
-            const filter = { _id: ObjectId(id) }
-            const updateDoc = {
-                $set: { ...product }
-            };
-            const result = await productCollection.updateOne(filter, updateDoc);
-            res.json(result);
-        })
-
-        //DELETE
-
-        //DELETE BOOKING
-        app.delete('/students/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await studentCollection.deleteOne(query);
-            res.json(result);
-        }); */
+ */
 
     }
     finally {
